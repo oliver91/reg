3ba158e262f24df2fcdedfbf5670fe981d50db2a
oliver91 <oliver91@ukr.net>
2014-10-15 22:54:35 +0300

    authenticate

--- app/models/User.java
+++ app/models/User.java
@@ -1,7 +1,30 @@
+
 package models;
 
-/**
- * Created by oliver on 15.10.14.
- */
-public class User {
+import play.db.ebean.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User extends Model {
+
+    @Id
+    public String email;
+    public String name;
+    public String password;
+
+    public User(String email, String name, String password) {
+        this.email = email;
+        this.name = name;
+        this.password = password;
+    }
+
+    public static User authenticate(String email, String password) {
+        return find.where().eq("email", email).eq("password", password)
+                .findUnique();
+    }
+
+    public static Finder<String, User> find = new Finder<String, User>(
+            String.class, User.class);
 }
